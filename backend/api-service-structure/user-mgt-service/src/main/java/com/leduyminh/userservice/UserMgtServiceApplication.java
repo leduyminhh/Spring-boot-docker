329312cbe4be8245f0b3defa37c1005d3885d7bd
MinhLD21 <minhld21@fpt.com.vn>
2022-12-29 11:53:24 +0700

    + Add config services

---
+++ backend/api-service-structure/user-mgt-service/src/main/java/com/leduyminh/userservice/UserMgtServiceApplication.java
@@ -0,0 +1,13 @@
+package com.leduyminh.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMgtServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMgtServiceApplication.class, args);
+	}
+
+}
